ad30b884e63efa7727a3ba36fe910f2b2e187159
Tyler Kandarian <tylerkandarian@tylers-mbp.attlocal.net>
2023-01-30 14:35:45 -0500

    trying to get page to load

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
